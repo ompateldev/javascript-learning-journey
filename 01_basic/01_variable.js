53337e9800fc6b36210fbb5912997f2c788de40e
ompateldev <bhojaniom2005@gmail.com>
2025-08-04 21:10:04 +0530

    Added basic JavaScript files in 01_basic folder

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+let a = 1;
+let accountEmail = "acb@gamil.com"
+const accountPassword = "12345"
+accountCity = "Delhi"
+
+console.log(accountEmail);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([a, accountEmail, accountPassword, accountCity])
---
+++ 01_basic/02_data_type.js
@@ -0,0 +1,25 @@
+
+
+//alert(2 + 2) we are useing node js not browser
+
+
+//number => 2 to power 53
+// String => ""
+// boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => unique
+
+console.log("Hello World");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+console.log(typeof isLoggedIn); //boolean
+console.log(typeof name); //string
+console.log(typeof number); //number
+
+
+
+
+
+
+
