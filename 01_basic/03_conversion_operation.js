02916cf07363edecb77bad209dde58c5c5f6102d
ompateldev <bhojaniom2005@gmail.com>
2025-08-05 07:12:57 +0530

    conversion_opration

---
+++ 01_basic/03_conversion_operation.js
@@ -0,0 +1,28 @@
+let score = "33abc"
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+//console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+
+let isLoggedIn = "om"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "om" => true
+
+let someNumber = 33
+
+let stringnumber = String(someNumber)
+
+console.log( typeof( stringnumber))
