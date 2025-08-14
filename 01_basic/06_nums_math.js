7652035f7ba4993a1b31b17a226627ad76ee0be0
ompateldev <bhojaniom2005@gmail.com>
2025-08-14 09:42:35 +0530

    Added numbers and math basics file

---
+++ 01_basic/06_nums_math.js
@@ -0,0 +1,53 @@
+const score = 400;
+
+const balance = new Number(103.9523);
+const balance2 = new Number(200);
+
+
+// console.log(typeof balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(4));
+
+
+const otherNumber = 1000000;
+
+// console.log(otherNumber.toLocaleString());
+
+// ++++++++++++++++ maths ++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+// console.log(Math.random());
+// console.log(Math.random() * 10);
+console.log((Math.random() * 10) + 1);
+
+const max = 20;
+const min = 10;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
