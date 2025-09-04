237ea44f2c56f84405e1f4eba344ba8e38660521
ompateldev <bhojaniom2005@gmail.com>
2025-09-04 10:01:57 +0530

    Add control flow basics file

---
+++ 04_basic/01_control_fow.js
@@ -0,0 +1,74 @@
+// < ,>,<=,>=,==,===,!=,!==
+
+let temperature = 25;
+let b = 35;
+
+// if(temperature != b) {
+//     console.log("temperature is not equal to b");
+// } else {
+//     console.log("b is equal to temperature");
+// }
+
+// let score = 75;
+
+// if (score <= 90) {
+//     const power = "fly";
+//     console.log(`you are a ${power} man`);
+// }
+
+// console.log(`your score is ${power}`);
+
+// let balance = 5000;
+
+// if (balance < 500) {
+//   console.log("less than 500");
+// } else if (balance < 1000) {
+//   console.log("less than 1000");
+// } else {
+//   console.log("greater than or equal to 1000");
+// }
+
+let userloggedin = true;
+let paymentmode = false;
+
+// if(userloggedin && paymentmode) {
+//     console.log("you can make a purchase");
+// } else {
+//     console.log("you cannot make a purchase");
+// }
+
+// if(userloggedin || paymentmode) {
+//     console.log("you can make a purchase");
+// } else {
+//     console.log("you cannot make a purchase");
+// }
+
+// switch (key) {
+//   case value:
+//     break;
+
+//   default:
+//     break;
+// }
+
+let month = "mar";
+
+switch (month) {
+  case "jan":
+    console.log("january");
+    break;
+  case "feb":
+    console.log("fenruary");
+    break;
+  case "mar":
+    console.log("march");
+    break;
+  case "apr":
+    console.log("april");
+    break;
+
+    
+  default:
+    console.log("january");
+    break;
+}
