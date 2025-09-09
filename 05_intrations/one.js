c37a0153115ae53ac5a3efd13d13a74ecd0747ac
ompateldev <bhojaniom2005@gmail.com>
2025-09-10 00:38:48 +0530

    Update one.js and add two.js

--- 05_intrations/one.js
+++ 05_intrations/one.js
@@ -1,8 +1,30 @@
-let n = 7;
+// let n = 7;
 
-for (let i = 0; i <= 10; i++) {
- 
-    let table = n * i;
-    console.log(`${n} * ${i} = ${table}`);
+// for (let i = 0; i <= 10; i++) {
+//   let table = n * i;
+//   console.log(`${n} * ${i} = ${table}`);
+// }
 
+// for (let index = 0; index <= 10; index++) {
+//   const element = index;
+
+//   if (element % 2 === 0) {
+//     console.log(`${element} this number is divisible `);
+//   }
+
+//   console.log(element);
+// }
+
+
+for (let index = 0; index <=10 ; index++) {
+    const element = index;
+
+    if( element == 2){
+        console.log(`${element} this number is detected`);
+        continue
+    }
+
+    console.log(element);
+    
+    
 }
---
+++ 05_intrations/two.js
@@ -0,0 +1,25 @@
+let index = 0;
+
+// while (index <= 10) {
+//   console.log(`i hate girl ${index}`);
+//   // index++;
+//   index = index + 2;
+// }
+
+// let myarry = ["apple", "banana", "orange", "grape"];
+
+// let arry = 0;
+// while (arry < myarry.length) {
+//   console.log(`I love ${myarry[arry]}`);
+//   arry = arry + 1;
+// }
+
+
+let socre = 11;
+
+do {
+  
+  console.log(`socre is  ${socre}`);
+  socre ++;
+
+} while (socre <= 10);
