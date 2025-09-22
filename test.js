80c4f299455b7fe39fc092155ced65263f46c58c
ompateldev <169343226+ompateldev@users.noreply.github.com>
2025-09-22 13:23:27 +0530

    Updated test.js and added 06_dom folder

--- test.js
+++ test.js
@@ -12,3 +12,9 @@ console.log(s2);    // "HITESH"
 
 
 
+const person = { name: "Om", age: 20 };
+person.age = 21; // ✅ allowed
+console.log(person); // { name: "Om", age: 21 }
+
+// person = { name: "Patel" }; // ❌ Error (reassign नहीं कर सकते)
+console.log(person.age);
