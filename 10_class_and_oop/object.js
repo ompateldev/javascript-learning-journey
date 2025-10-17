22dc4f2b8bafa8073c26d128084191b203238121
ompateldev <169343226+ompateldev@users.noreply.github.com>
2025-10-17 08:55:45 +0530

    object.js updated with new code

--- 10_class_and_oop/object.js
+++ 10_class_and_oop/object.js
@@ -7,3 +7,19 @@ multipaby5.power = 2;
 console.log(multipaby5(5));
 console.log(multipaby5.power);
 console.log(multipaby5.prototype);
+
+function CreateUser(username, score) {
+  this.username = username;
+  this.score = score;
+}
+
+CreateUser.prototype.increment = function () {
+  this.score++;
+};
+
+CreateUser.prototype.printMe = function(){
+  console.log(`username: ${this.username}, score: ${this.score}`);
+}
+
+const chai =  CreateUser("chai", 3);
+const tea =  CreateUser("tea", 5);
