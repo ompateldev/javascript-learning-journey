f6dffdcdce2999472ee976701c7b86cac78ada89
ompateldev <169343226+ompateldev@users.noreply.github.com>
2025-10-28 14:35:58 +0530

    Updated prototype example

--- 10_class_and_oop/prototypes.js
+++ 10_class_and_oop/prototypes.js
@@ -1,5 +1,58 @@
+// let myname = 'Alice     ';
 
-let myname = 'Alice     ';
+// console.log(myname.trueLength); // undefined
+// console.log(myname.trim().length);
 
-console.log(myname.trueLength); // undefined
-console.log(myname.trim().length);
+let herosName = ["thor", "spiderman"];
+
+let herosPower = {
+  thor: "hamer",
+  spiderman: "sling",
+
+  getspiderPower: function () {
+    console.log(`spidy man power is ${this.spiderman}`);
+  },
+};
+
+Object.prototype.om = function () {
+  console.log(`om is all of objects`);
+};
+
+Array.prototype.heyom = function () {
+  console.log("hey om this side ");
+};
+
+// herosPower.om();
+// herosPower.heyom();
+// herosName.heyom();
+
+const user = {
+  name: "om",
+  email: "om@example.com",
+};
+
+const Teacher = {
+  makVideo: true,
+};
+const TeachSupport = {
+  isAvailable: false,
+};
+
+const TeachingSupport = {
+  makeAssignment: "js assignment",
+  fulltime: true,
+  __proto__: TeachSupport,
+};
+
+// TeachingSupport.__proto__ = TeachSupport;
+
+let StringOne = "omkar    ";
+
+// console.log(StringOne.trim().length);
+
+String.prototype.truLenth = function () {
+  console.log(`${this.length}`);
+  console.log(`true lenth is ${this.trim().length}`);
+};
+
+StringOne.truLenth();
